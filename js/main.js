8a9de2cab8c29b1e19ccb3cc23524a89520c3b4e
Miguel Placido Felix <164278747+Miguelfelix16@users.noreply.github.com>
2024-11-12 00:52:44 +0000

    11/11/24

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Miguel Nutricionista";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Pacientes Cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
